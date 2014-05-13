51893473b0edf4652e4a07b3940a367e455c2b1f
Andrew O'Harney <andrew0harney@users.noreply.github.com>
2014-05-13 13:46:56 -0400

    Update GridRegression.py
    Fully commented for pydoc

--- GridRegression.py
+++ GridRegression.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('__GridRegression__')
   
 """Functions and classes for performing online regression over data"""
-__author__ =  'Andrew O\Harney'
+__author__ =  'Andrew O\'Harney'
   
 
 """Abstract Classes"""
@@ -26,7 +26,6 @@ class Encoder():
         "Return the number of classes(topics) in data"
         pass
 
-#
 class model():
     """Base model class for all online Regression methods"""
     __metaclass__ = ABCMeta
@@ -97,6 +96,13 @@ class GridRegression:
     __model__ = None           
     
     def __init__(self,model,grid,nlags=0,noiseOrders=None,encoding=None):
+        """Keyword Arguments:
+        model -- ML model to use for regression. Must be of type GridRegression.model
+        grid -- Grid object containing data
+        nlags -- Number of lags to use for each encoding category
+        noiseOrders=None -- Polynomial orders of noise
+        encoding=None -- Encoder for given event. Must be of type GridRegression.Encoder"""
+        
         self.setNumLags(nlags)
         self.setGrid(grid)
         self.setNoiseOrders(noiseOrders)
@@ -104,81 +110,128 @@ class GridRegression:
         self.setnClasses(encoding.numClasses())
         self.setnPoints(self.nlags()*self.nClasses()+self.nClasses()+len(self.noise().columns)) #Number of columns in design matrix
         self.setModel(model)
-    #Getters
+    
+    """Getters"""
     def grid(self):
+        """Return grid in use"""
         return self.__grid__
     def noise(self):
+        """Return noise matrix"""
         return self.__noise__
     def nClasses(self):
+        """Return number of classes in data"""
         return self.__nClasses__
     def nPoints(self):
+        """Return the number of encoding points in the design matrix (not including noise)"""
         return self.__nPoints__
     def noiseOrders(self):
+        """Polynomial order of noise"""
         return self.__noiseOrders__
     def nlags(self):
+        "Number of lags for each encoding category"
         return self.__nlags__
     def coefs(self):
+        """Inferred Coefficients"""
         #Inferred parameters
         return self.__model__.coefs()
     def events(self):
+        """Events used for training"""
         #Current events in use
         return self.__events__
     def longestEvent(self):
+        """Longest event in the training set"""
         #Longest event in samples
         return self.__longestEvent__
     def encoding(self):
-        #Encoding matrix for gesign
+        """Encoding object (GridRegression.Encoder)"""
         return self.__encoding__
     def model(self):
+        """Model in use"""
         return self.__model__
     def alphaH(self):
+        """Inferred parameters for encoding columns"""
         return self.coefs()[:-self.noise().shape[1],:]
     def betaH(self):
+        """Inferred parameters for noise columns"""
         return self.coefs()[self.noise().shape[1]:,:]
-    #Setters
+    
+    """Setters"""
     def setEvents(self,events):
+        """Set events to be used for training
+        Keyword Arguments:
+        events -- Events to be used for training"""
         logger.info( 'Setting events')
         self.__events__ = events
     def setNoise(self,noise):
+        """Set noise matrix
+        noise -- Noise matrix to use"""
         logger.info( 'Setting noise matrix')
         #Noise should be a matrix of length=grid.times() describing noise in the signal over time
         self.__noise__ = noise
     def setnClasses(self,nclasses):
+        """Set number of classes in data
+        Keyword Arguments:
+        nclasses -- Number of classes (n.b this does not include lag columns)"""
         self.__nClasses__ = nclasses
     def setnPoints(self,npoints):
+        """Set the number of points used for encoding columns
+        Keyword Arguments:
+        npoints -- Number of points for encoding columns (not including noise columns)"""
         self.__nPoints__ = npoints
     def setCoefs(self,coefs):
+        """Set the coefficients to be used by the model
+        Keyword Arguments:
+        coefs -- Weighting parameters for each row of the design matrix"""
         logger.info( 'Setting coefficients')
         self.__model__.setCoefs(coefs)
     def setGrid(self,grid):
+        """Sets the grid in use
+        Keyword Arguments:
+        grid -- Grid to take data from"""
         logger.info( 'Setting grid')
         #Change grid and update noise matrix if necessary
         self.__grid__ = grid
     def setNumLags(self,numLags):
+        """Set the number of lag columns
+        Keyword Arguments:
+        numLags: Number of lags to be used for each event class"""
         logger.info( 'Setting number of lags')
         self.__nlags__ = numLags
-    def setLambda(self,lmbda):
         self.__lambda__ = lmbda
     def setModel(self,model):
+        """Set the model to be used for regression
+        Keyword Arguments:
+        model -- Any model inheriting GridRegression.model"""
         self.__model__ = model
-    def setNoiseOrders(self,noiseOrders):
+    def setNoiseOrders(self,noiseOrder):
+        """Set the polynomial orders for noise
+        Keyword Arguments:
+        noiseOrders -- Maximum polynomial order to use for noise"""
         logger.info( 'Setting polynomial orders for noise matrix')
         #Set maximum order of noise and update the matrix
-        self.__noiseOrders__= noiseOrders
+        self.__noiseOrders__= noiseOrder
         self.setNoise(self.genNoise(self.grid(), self.noiseOrders()))
 
     def setLongestEvent(self,longestEvent):
+        """Set the longest event to consider in training and testing
+        Keyword Arguments:
+        longestEvent: Longest event period to consider in seconds"""
         logger.info( 'setting longest event time')
         self.__longestEvent__ = int(longestEvent*self.grid().fs())
         
     def setEncoding(self,encoding):
+        """Set the encoder to be used for event encodings
+        Keyword Arguments:
+        encoding -- GridRegression.Encoder object"""
         logger.info( 'Setting encoding to be used in design matrix')
         self.__encoding__ = encoding
     
-    #ML functions
+    """Machine learning functions"""
     def genNoise(self,grid,maxNoiseOrder):
-        #Noise is a matrix of Legendre polynomials of 0<order<maxNoiseOrder
-        #Additionally 60Hz sine and cosine waves are added to account for the DC component of EEG
+        """Noise is a matrix of Legendre polynomials of 0<order<maxNoiseOrder
+        Additionally 60Hz sine and cosine waves are added to account for the DC component of EEG
+        grid -- Grid to be used for timing information
+        maxNoiseOrder--Maximum order of noise to be considered"""
 
         if self.grid() is not None and self.noiseOrders() is not None:
             logger.info( 'Generating noise matrix')
@@ -190,10 +243,11 @@ class GridRegression:
 
 
     def event_design(self,event,encoding=None,prevCode=None,longest=None):
-        #Generates the design matrix for an event
-        #encoding - class that returns the encoding of a given event as a series {topicNumber:probability}
-        #prevCode - is design is dependent on a previous state then pass it here       
-        #longest - longest event in seconds
+        """Generates the design matrix for an event
+        Keyword Arguments:
+        encoding=None - Encoder Class (GridRegression.Encoder) that returns the encoding of a given event as a series {topicNumber:value}
+        prevCode=None - If design is dependent on a previous state then pass it here
+        longest=None - longest event in seconds (Defualt longest event in training)"""
         
         if encoding is None:
             encoding = self.encoding()
@@ -231,7 +285,9 @@ class GridRegression:
         return pd.DataFrame(design,index=times)
             
     def genX(self,events):
-        #Generates the event matrix for events (in memory). Use with caution!     
+        """Generates the event matrix for events (in memory). Use with caution!
+        Keyword Arguments:
+        events -- Events to generate design matrix for"""
         from tempfile import mkdtemp
         import os.path as path
                        
@@ -261,7 +317,10 @@ class GridRegression:
         return X
     
     def __iterX__(self,events):      
-        #Generator for event matrix blocks
+        """Generator for event matrix blocks
+        Returns an iterator for event designs (better than in memory construction of the whole design)
+        Keyword Arguments:
+        events -- Events to generate design matrix for"""
         
         encoding = self.encoding()             
         longest=self.longestEvent()      
@@ -297,13 +356,15 @@ class GridRegression:
                 #prevCode = design
             yield design,times
                 
-    ###########Training functions###################
     def train(self,events,y,encoding=None,longest=None,normalise=None):
         """Online training of regression over events 
-        Encoding -- Dictionary that has row encodings for a given label(not including lags)
-        Longest -- Limit the maximum event length in seconds
-        Normalisation -- Normalisation coefficients to apply to columns of the design {zscore,l1,l2}
-        Model -- Type of model to use {linear,SGD}"""
+        Keyword Arguments:
+        events -- events to train from 
+        y -- training data
+        encoding=None -- Dictionary that has row encodings for a given label(not including lags)
+        longest=None -- Limit the maximum event length in seconds
+        normalisation=None -- Normalisation coefficients to apply to columns of the design {zscore,l1,l2}
+        """
         model = self.model()
         
         if longest is not None:
@@ -340,7 +401,10 @@ class GridRegression:
         return model.coefs()
     
     def predict(self,events,coefs=None):
-
+        """Perform prediction
+        Keyword Arguments:
+        events -- Events to predict signal for
+        coefs=None -- Design weightings to be used for prediction (defualt is those inferred from training)"""
         if coefs is None:
             coefs= self.coefs()
         self.setCoefs(coefs)
